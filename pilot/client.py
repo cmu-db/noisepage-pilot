8bc52531cd8a6a2d63abb98894c6f794d2b48473
Wan Shen Lim <wanshen.lim@gmail.com>
2022-01-05 05:39:54 -0500

    Add the Pilot component. (#10)
    * Add task_bootstrap. Small fixes to dodo.py.
    
    - The bootstrap task sets up databases and users.
    - Fix params passing for dodo.py; long name shouldn't have preceding --.
    - Small style refactor of dodo.py.
    
    * Add basic Pilot daemon/client implementation.
    
    - This work is based off mkpjnx's branch.
    - Uses LISTEN/NOTIFY as basic job server.
    - Protocol is defined in protocol.py.
    - Adds tasks:
      - pilot_bootstrap: bootstrap pilot tables.
      - pilot_daemon: run Pilot server.
      - pilot_client: invoke job on Pilot.
    
    
    Co-authored-by: mkpjnx <mikepjxu@gmail.com>

---
+++ pilot/client.py
@@ -0,0 +1,47 @@
+import csv
+from io import StringIO
+
+from plumbum import cli
+from protocol import Client
+
+
+class ClientCLI(cli.Application):
+    db_conn_string = cli.SwitchAttr(
+        "--db-conn-string",
+        str,
+        mandatory=True,
+        help="Connection string to Pilot database.",
+    )
+    command = cli.SwitchAttr(
+        "--command",
+        str,
+        mandatory=True,
+        help="The Pilot Daemon command to be invoked.",
+    )
+
+    def main(self, *args):
+        # Get the function to be invoked that will
+        # send a command to the Pilot Daemon.
+        client = Client(self.db_conn_string)
+        func = client.get_function(self.command)
+
+        # Get the arguments to the function, if any.
+        data = {}
+        assert len(args) in [0, 1]
+        if len(args) == 1:
+            # TODO(WAN): We assume a CSV string of input.
+            iostr = StringIO(args[0])
+            reader = csv.reader(iostr)
+            data = {
+                key: val
+                for row in reader
+                for item in row
+                for key, val in [item.split("=", maxsplit=1)]
+            }
+
+        # Invoke the function.
+        func(data)
+
+
+if __name__ == "__main__":
+    ClientCLI.run()
