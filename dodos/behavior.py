7a2f68b044386c29cef7856d0dad1fa77fd9056a
Wan Shen Lim <wanshen.lim@gmail.com>
2022-01-20 06:31:23 -0500

    Add model inference via a local Flask app. (#24)
    Add model inference via a local Flask app.
    
    - Add `flask` as a dependency.
    - Add `behavior_microservice` task.
    - Add "Y" as a valid variable name since we already have "x", "X", "y" anyway.
    - Save the entire BehaviorModel to maintain labels for features and targets.
    
    Sample usage:
    
    - If you have no models, run `behavior_datagen` and `behavior_train` first.
    - `doit behavior_microservice`
      - This uses the latest set of models by default.
      - You can see the output being redirected to `behavior_microservice.out`.
    - Go to `http://localhost:5000/model/<MODEL_TYPE>/<MODEL_NAME>/?<FEATURES>`
      - For example, `http://127.0.0.1:5000/model/dt/SeqScan/?total_cost=123&plan_width=123&startup_cost=123&diffed_total_cost=123&Scan_plan_parallel_safe=0`

--- dodos/behavior.py
+++ dodos/behavior.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from plumbum import local
+
 import dodos.benchbase
 import dodos.noisepage
 from dodos import VERBOSITY_DEFAULT, default_artifacts_path, default_build_path
@@ -78,5 +80,28 @@ def task_behavior_train():
             f"python3 -m behavior train {train_args}",
         ],
         "targets": [ARTIFACT_MODELS],
+        "verbosity": VERBOSITY_DEFAULT,
+    }
+
+
+def task_behavior_microservice():
+    """
+    Behavior modeling: models as a microservice (via Flask).
+    """
+
+    def run_microservice():
+        # Find the latest experiment.
+        # TODO(WAN): Make this configurable.
+        experiment_list = sorted(exp_path for exp_path in ARTIFACT_MODELS.glob("*"))
+        assert len(experiment_list) > 0, "No experiments found."
+        experiment_name = experiment_list[-1]
+
+        server_cmd = local["python3"]["-m", "behavior", "microservice", "--models-path", experiment_name]
+        dest_stdout = "behavior_microservice.out"
+        ret = server_cmd.run_nohup(stdout=dest_stdout)
+        print(f"Behavior models microservice: {dest_stdout} {ret.pid}")
+
+    return {
+        "actions": [run_microservice],
         "verbosity": VERBOSITY_DEFAULT,
     }
