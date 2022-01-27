8741f1324def2f971fda41f26509623d1d8c0def
Wan Shen Lim <wanshen.lim@gmail.com>
2022-01-27 18:11:45 -0500

    Update mvp.sh script.

--- demo/mvp.sh
+++ demo/mvp.sh
@@ -3,8 +3,13 @@ set -ex
 # Various steps may require sudo.
 sudo --validate
 
+# Increase file descriptor limit for TScout.
+ulimit -n 8096
+
 # Clean up the result of previous runs, if any.
 doit ci_clean_slate
+sudo pkill postgres || true
+doit behavior_microservice_kill
 
 # Behavior models need to be trained.
 # Because the behavior code was written prior to pilot/dodo,
@@ -26,10 +31,13 @@ doit noisepage_enable_logging
 doit benchbase_run --benchmark="tpcc" --args="--execute=true"
 doit noisepage_disable_logging
 
+# Because BenchBase scales down the workload, we would end up predicting nothing by default.
+# We hack around it by deleting the last few query log files.
+doit noisepage_truncate_log
+
 # Predict the forecast manually because we need to control the time for now.
 # Otherwise it would default to predicting [now, now+1 minute].
-# TODO(WAN): Eventually, figure out the truncation problem.
-doit forecast_predict --time_start='2022-01-20 08:05:00 EST' --time_end='2022-01-20 08:30:00 EST'
+doit forecast_predict
 
 ##############################################################################
 # After this line, both forecast and behavior models should be ready.
@@ -48,12 +56,13 @@ doit action_recommendation_bootstrap_dbms
 
 # We need to load the same schema as the forecast.
 # TODO(WAN): Editing XML can be such a pain...
+mkdir -p artifacts/demo/
 sudo apt-get install xmlstarlet
-cp ./config/behavior/benchbase/tpcc_config.xml ./demo/mvp_tpcc_config.xml
-xmlstarlet edit --inplace --update '/parameters/url' --value 'jdbc:postgresql://localhost:5432/np_as_spiel?preferQueryMode=extended' ./demo/mvp_tpcc_config.xml
-xmlstarlet edit --inplace --update '/parameters/username' --value 'np_as_spiel_user' ./demo/mvp_tpcc_config.xml
-xmlstarlet edit --inplace --update '/parameters/password' --value 'np_as_spiel_pass' ./demo/mvp_tpcc_config.xml
-doit benchbase_run --benchmark="tpcc" --config="./demo/mvp_tpcc_config.xml" --args="--create=true --load=true"
+cp ./config/behavior/benchbase/tpcc_config.xml ./artifacts/demo/mvp_tpcc_config.xml
+xmlstarlet edit --inplace --update '/parameters/url' --value 'jdbc:postgresql://localhost:5432/np_as_spiel?preferQueryMode=extended' ./artifacts/demo/mvp_tpcc_config.xml
+xmlstarlet edit --inplace --update '/parameters/username' --value 'np_as_spiel_user' ./artifacts/demo/mvp_tpcc_config.xml
+xmlstarlet edit --inplace --update '/parameters/password' --value 'np_as_spiel_pass' ./artifacts/demo/mvp_tpcc_config.xml
+doit benchbase_run --benchmark="tpcc" --config="./artifacts/demo/mvp_tpcc_config.xml" --args="--create=true --load=true"
 # And then we need to drop those two TPC-C indexes.
 PGPASSWORD=np_as_spiel_pass ./artifacts/noisepage/psql -h localhost -d np_as_spiel -U np_as_spiel_user -c "DROP INDEX idx_customer_name;"
 PGPASSWORD=np_as_spiel_pass ./artifacts/noisepage/psql -h localhost -d np_as_spiel -U np_as_spiel_user -c "DROP INDEX idx_order;"
